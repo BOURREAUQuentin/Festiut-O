ef74b4378ae8ca55e473fbb8cc8dd3b7a17d6b3a
Quentin_BOURREAU <quentin.bourreau@etu.univ-orleans.fr>
2024-01-02 19:26:41 +0100

    feat: finition de la classe Spectateur + modif nom variables classe Artiste

--- src/modele/python/artiste.py
+++ src/modele/python/artiste.py
@@ -1,23 +1,23 @@
 class Artiste:
     def __init__(self, id, nom, age, description):
-        self.__idA = id
-        self.__nomA = nom
-        self.__descriptionA = description
+        self.__id_a = id
+        self.__nom_a = nom
+        self.__description_a = description
 
     def get_id(self):
-        return self.__idA
+        return self.__id_a
 
     def get_nom(self):
-        return self.__nomA
+        return self.__nom_a
 
     def get_description(self):
-        return self.__descriptionA
+        return self.__description_a
     
     def set_nom(self, nouveau_nom):
-        self.__idA = nouveau_nom
+        self.__id_a = nouveau_nom
 
     def set_description(self, nouvelle_description):
-        self.__descriptionA = nouvelle_description
+        self.__description_a = nouvelle_description
 
     def __str__(self):
-        return f"id artiste : {self.__idA}, le nom : {self.__nomA}, la description : {self.__descriptionA}"
+        return f"id artiste : {self.__id_a}, le nom : {self.__nom_a}, la description : {self.__description_a}"
--- src/modele/python/spectateur.py
+++ src/modele/python/spectateur.py
@@ -1,22 +1,59 @@
 class Spectateur:
-    def __init__(self, id, nom, prenom, mail, dateNaiss, tel, nomUtilisateur, mdp, admin):
-        self.__idS = id
-        self.__nomS = nom
-        self.__prenomS = prenom
-        self.__mailS = mail
-        self.__dateNaissS = dateNaiss
-        self.__telS = tel
-        self.__nomUtilisateurS = nomUtilisateur
-        self.__mdpS = mdp
-        self.__adminS = admin
+    def __init__(self, id, nom, prenom, mail, date_naissance, tel, nom_utilisateur, mdp, admin):
+        self.__id_s = id
+        self.__nom_s = nom
+        self.__prenom_s = prenom
+        self.__mail_s = mail
+        self.__date_naissance_s = date_naissance
+        self.__tel_s = tel
+        self.__nom_utilisateur_s = nom_utilisateur
+        self.__mdp_s = mdp
+        self.__admin_s = admin
 
     def get_id(self):
-        return self.__idS
+        return self.__id_s
 
     def get_nom(self):
-        return self.__nomS
+        return self.__nom_s
 
+    def get_prenom(self):
+        return self.__prenom_s
 
+    def get_mail(self):
+        return self.__mail_s
 
+    def get_date_naissance(self):
+        return self.__date_naissance_s
+
+    def get_telephone(self):
+        return self.__tel_s
+
+    def get_nom_utilisateur(self):
+        return self.__nom_utilisateur_s
+
+    def get_mdp(self):
+        return self.__mdp_s
+
+    def get_admin(self):
+        return self.__admin_s
+
+    def set_nom(self, nouveau_nom):
+        self.__nom_s = nouveau_nom
+
+    def set_prenom(self, nouveau_prenom):
+        self.__prenom_s = nouveau_prenom
+
+    def set_mail(self, nouveau_mail):
+        self.__mail_s = nouveau_mail
+    
+    def set_telephone(self, nouveau_tel):
+        self.__tel_s = nouveau_tel
+    
+    def set_nom_utilisateur(self, nouveau_nom_utilisateur):
+        self.__nom_utilisateur_s = nouveau_nom_utilisateur
+    
+    def set_mdp(self, nouveau_mdp):
+        self.__mdp_s = nouveau_mdp
+    
     def __str__(self):
-        return f"id artiste : {self.__idA}, le nom : {self.__nomA}, la description : {self.__descriptionA}"
+        return f"id spectateur : {self.__id_s}, le nom : {self.__nom_s}, le prenom : {self.__prenom_s}, le nom utilisateur : {self.__nom_utilisateur_s}"
