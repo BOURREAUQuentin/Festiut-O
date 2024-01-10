4c9bfd3cbd6227d98846e1dede410c2e8bd37ed3
MaverickPrevost <maverickprevost@gmail.com>
2024-01-10 15:25:10 +0100

    feat : Ajout de la liaison entre la bd et la page des groupes

--- src/modele/python/groupe.py
+++ src/modele/python/groupe.py
@@ -1,8 +1,9 @@
 class Groupe:
-    def __init__(self, id, nom, description):
+    def __init__(self, id, nom, description, lien_image):
         self.__id_g = id
         self.__nom_g = nom
         self.__description_g = description
+        self.__lien_image_g = lien_image
 
     def get_id(self):
         return self.__id_g
@@ -12,6 +13,9 @@ class Groupe:
 
     def get_description(self):
         return self.__description_g
+    
+    def get_lien_image(self):
+        return self.__lien_image_g
 
     def set_nom(self, nouveau_nom):
         self.__nom_g = nouveau_nom
@@ -20,4 +24,4 @@ class Groupe:
         self.__description_g = nouvelle_description
 
     def __str__(self):
-        return f"id groupe : {self.__id_g}, le nom : {self.__nom_g}, la description : {self.__description_g}"
+        return f"id groupe : {self.__id_g}, le nom : {self.__nom_g}, la description : {self.__description_g}, le lien de l'image : {self.__lien_image_g}"
